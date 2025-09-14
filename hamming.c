a8d7eae97838e6506009a05c803cedcfdf2a9638
Nikolaj Gundstrup <gunniko@gmai.com>
2025-09-14 07:00:20 +0200

    hamming distance func

--- hamming.c
+++ hamming.c
@@ -1,6 +1,16 @@
-#include <stdio>
+#include <stdio.h>
+#include <string.h>
+#include "hamming.h"
 
-
-int hamming_distance(char dna1[], char dan2[]){
-
+int hamming_distance(char dna1[], char dna2[]){
+  int count = 0;
+  int len = strlen(dna1);
+  
+  for(int i = 0; i < len; i++){
+    if(dna1[i] != dna2[i]){
+      count++;
+    }
+  }
+  
+  return count;
 }
